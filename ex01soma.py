e5139db8d0053a2f1af7629b28f3ce9ac225070a
Alysson Naves <anaves@gmail.com>
2023-02-23 21:54:32 -0300

    aula 23-02-2023

---
+++ ex01soma.py
@@ -0,0 +1,17 @@
+# Pseudocodigo 
+# Leia A
+# Leia B
+# SOMA <- A + B
+# Escreva SOMA
+
+#----------------------- 
+# entrada de dados
+A = input('Digite primeiro valor:')   #A = 5
+A = int(A)
+B = int(input("Digite segundo valor: "))   #B = 7
+# processamento
+SOMA = A + B
+# saida de dados
+print(SOMA)  
+
+
---
+++ ex02avaliar.py
@@ -0,0 +1,7 @@
+# entrada de dados
+A = int(input('Primeiro valor: '))
+B = int(input('Segundo valor: '))
+# processamento
+R = A > B   # avaliar
+# saida
+print(R)
