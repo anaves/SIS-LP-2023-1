ca9a9cc2f766029e5dd67883b01175fee3a8cb7f
Alysson Naves <anaves@gmail.com>
2023-05-18 19:56:54 -0300

    aula 18-05-2023 + lista02 - repeticao

--- aulas/aula013_18_05_2023/fibonacci.py
+++ aulas/aula013_18_05_2023/fibonacci.py
@@ -3,28 +3,19 @@ Faça um programa para calcular a série de Fibonacci para um número informado 
 Por exemplo, caso o usuário informe o número 9, o resultado seria:
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 """
-
+N = int(input("Digite ate a posicao desejada: "))
+# Casos iniciais
 a = 0
 b = 1
 # inicio
-print(a, end=', ')
-print(b, end=', ')
-" geracao 2"
-c = a + b
-print(c, end = ", ")
-# atualizar
-a = b
-b = c
-# geracao 3
-c = a+b
-print(c, end = ", ")
-# atualizar
-a = b
-b = c
-# geracao 4
-c = a+b
-print(c, end = ", ")
-# atualizar
-a = b
-b = c
+if N >= 0:
+    print(a, end=', ')
+if N >= 1:
+    print(b, end=', ')
+for geracao in range(2,N+1):
+    c = a + b
+    print(c, end = ", ")
+    # atualizar anteriores
+    a = b
+    b = c
 print()
