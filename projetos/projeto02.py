aeac791e982c55aa1c4be5f54bfd3cd924689a27
Alysson Naves <anaves@gmail.com>
2023-06-21 19:50:21 -0300

    projetos02 e 03

---
+++ projetos/projeto02.py
@@ -0,0 +1,12 @@
+"""
+Codifique um algoritmo que exiba um histograma da variação da temperatura durante a semana. Por exemplo, se as temperaturas forem: 19, 21, 25, 22, 20, 17 e 15°C, de domingo a sábado, respectivamente, o algoritmo deverá exibir:
+D: *******************
+S: *********************
+T: *************************
+Q: **********************
+Q: ********************
+S: *****************
+S: *******************
+Suponha que as temperaturas sejam todas positivas e que nenhuma seja maior que 80°C.
+
+"""
---
+++ projetos/projeto03.py
@@ -0,0 +1,23 @@
+"""
+Projeto: Adivinhe o número
+
+Descrição: Neste jogo, o programa escolhe aleatoriamente um número entre 1 e 100. O jogador deve tentar adivinhar o número correto dentro de um número limitado de tentativas.
+
+Regras:
+
+O programa escolhe um número aleatório entre 1 e 100.
+O jogador tem no máximo 10 tentativas.
+O jogador faz uma suposição fornecendo um número.
+O programa informa ao jogador se o número correto é maior ou menor do que a suposição feita.
+O jogador continua fazendo suposições até adivinhar corretamente o número ou esgotar o número de tentativas.
+Funcionalidades adicionais:
+
+No início do jogo, o programa exibe uma mensagem de boas-vindas e explica as regras do jogo.
+O programa mantém um registro do número de tentativas do jogador.
+Ao final do jogo, o programa informa se o jogador adivinhou corretamente o número ou não, e exibe o número de tentativas realizadas.
+O programa pergunta se o jogador deseja jogar novamente.
+Dicas:
+
+Você pode usar a biblioteca random em Python para gerar o número aleatório.
+Utilize estruturas de controle como loops e condicionais para implementar a lógica do jogo.
+Lembre-se de validar a entrada do jogador, garantindo que seja um número dentro do intervalo correto."""
